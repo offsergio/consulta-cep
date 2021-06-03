6b392a485dc942bbce39966dc5c1d72fe003caa3
offsergio <offsergio@live.com>
2021-06-03 16:08:29 -0300

    add js button

---
+++ app.js
@@ -0,0 +1,14 @@
+var submitButton = document.querySelector('#app form button')
+var zipCodeField = document.querySelector('#app form input')
+var content = document.querySelector('#app main')
+
+submitButton.addEventListener('click', run)
+
+function run (event) {
+  event.preventDefault()
+  var zipCode =zipCodeField.value
+
+  zipCode = zipCode.replace(' ', '')
+  zipCode = zipCode.replace('.', '')
+  zipCode = zipcode.trim()
+}
